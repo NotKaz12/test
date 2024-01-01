addc8d8a65bc716a59cc1426e584b6c67d59f907
Jay <jaymarabistado723@gmail.com>
2024-01-01 21:54:44 +0800

    LAST NA TLGA

--- Main.java
+++ Main.java
@@ -1,24 +1,19 @@
-class SwapNumbers {
+import java.util.Scanner;
+
+class HelloWorld {
 
     public static void main(String[] args) {
 
-        float first = 1.20f, second = 2.45f;
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
 
-        System.out.println("--Before swap--");
-        System.out.println("First number = " + first);
-        System.out.println("Second number = " + second);
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
 
-        // Value of first is assigned to temporary
-        float temporary = first;
-
-        // Value of second is assigned to first
-        first = second;
-
-        // Value of temporary (which contains the initial value of first) is assigned to second
-        second = temporary;
-
-        System.out.println("--After swap--");
-        System.out.println("First number = " + first);
-        System.out.println("Second number = " + second);
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
+        //test lng hihi//
     }
 }
